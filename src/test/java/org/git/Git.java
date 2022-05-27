8c4a0ce290cd72d1254418aafd778dc444123e7e
rajendransaravanan <rajendransarvananofficial@gmail.com>
2022-05-27 22:08:27 +0530

    methodsadded

---
+++ src/test/java/org/git/Git.java
@@ -0,0 +1,28 @@
+package org.git;
+
+public class Git {
+
+	
+	private void rajendran() {
+		System.out.println("jakja");
+	}
+	
+	private void saravanan() {
+		System.out.println("jakja");
+	}
+	private void vinoth() {
+		System.out.println("reger");
+	}
+	private void kumar() {
+		System.out.println("ete");
+
+	}
+	private void yogesh() {
+		System.out.println("aete");
+	}
+	
+	
+	
+	
+	
+}
